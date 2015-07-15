3f231d5fb7b4396f56de611fc4da88e4ef6b5499
annazhukova <zhutchok@gmail.com>
2015-07-15 15:38:05 +0200

    moved cobra-related files from mod_sbml to mod_cobra

--- mod_sbml/serialization/serialization_manager.py
+++ mod_sbml/serialization/serialization_manager.py
@@ -6,13 +6,10 @@ import openpyxl
 
 from openpyxl.styles import Style, Font
 
-from mod_sbml.gibbs.gibbs_manager import get_r_kegg2equilibrium_const
-from mod_sbml.gibbs.reaction_boundary_manager import get_bounds, compare_kegg_formulas, NO_KEGG_ID, NO_KEGG_INFO_FOUND, \
-    get_better_r_bounds, REVERSED, SUCCESS
-from mod_sbml.kegg.reaction_manager import get_rn2kegg_formula, get_rn2compounds
+from mod_cobra.gibbs.reaction_boundary_manager import get_bounds
 from mod_sbml.sbml.sbml_manager import get_kegg_m_id, get_kegg_r_id, get_gene_association, get_r_comps, get_reactants, \
-    get_products, get_metabolites
-from mod_sbml.serialization.xlsx_helper import save_data, add_values, HEADER_STYLE, RED_STYLE, BASIC_STYLE
+    get_products
+from mod_sbml.serialization.xlsx_helper import save_data, add_values, HEADER_STYLE, BASIC_STYLE
 
 __author__ = 'anna'
 
@@ -30,94 +27,6 @@ def serialize_model_info(sbml, path, r_style=lambda r_id: BASIC_STYLE):
                for r in sorted(model.getListOfReactions(), key=lambda r: r.id)),
               styles=(r_style(r.id) for r in sorted(model.getListOfReactions(), key=lambda r: r.id)),
               ws_index=1, ws_name="Reactions", wb=wb)
-    wb.save(path)
-
-
-def serialize_info(model, r_ids, path):
-    m_ids = set()
-    wb = openpyxl.Workbook()
-    ws = wb.create_sheet(1, "Reactions")
-    add_values(ws, 1, 1,
-               ["Id", "Name", "Low. Bound", "Upp. Bound", "Formula", "KEGG Formula", "Kegg", "Genes",
-                "Ke(pH=7,T=298.15K)"],
-               HEADER_STYLE)
-    row = 2
-    kegg2formula = get_rn2kegg_formula()
-    k_formula = lambda kegg: kegg2formula[kegg] if kegg and kegg in kegg2formula else ''
-    kegg2ke = get_r_kegg2equilibrium_const(ph=7)
-    ke = lambda kegg: kegg2ke[kegg] if kegg in kegg2ke else ''
-    for r_id in r_ids:
-        r = model.getReaction(r_id)
-        lb, ub = get_bounds(r)
-        kegg = get_kegg_r_id(r)
-        values = [r.id, r.name, lb, ub, get_sbml_r_formula(model, r, False), k_formula(kegg), kegg,
-                  get_gene_association(r), ke(kegg)]
-        add_values(ws, row, 1, values)
-        row += 1
-        m_ids |= get_metabolites(r)
-    save_data(["Id", "Name", "Compartment", "Kegg"],
-              ((m.id, m.name, model.getCompartment(m.getCompartment()).name, get_kegg_m_id(m))
-               for m in (model.getSpecies(m_id) for m_id in m_ids)),
-              ws_index=0, ws_name="Metabolites", wb=wb)
-    wb.save(path)
-
-
-def serialize_info_with_pw(model, pw2r_ids, o_r_ids, pw2pw_name, path):
-    wb = openpyxl.Workbook()
-    save_data(["Id", "Name", "Compartment", "Kegg"],
-              ((m.id, m.name, model.getCompartment(m.getCompartment()).name, get_kegg_m_id(m))
-               for m in model.getListOfSpecies()),
-              ws_index=0, ws_name="Metabolites", wb=wb)
-    if o_r_ids:
-        pw2r_ids["No pathway"] = o_r_ids
-    kegg2ke = get_r_kegg2equilibrium_const(ph=7)
-    kegg2formula = get_rn2kegg_formula()
-    kegg2rs_ps = get_rn2compounds()
-    m2kegg = {m.id: get_kegg_m_id(m) for m in model.getListOfSpecies()}
-    k_formula = lambda kegg: kegg2formula[kegg] if kegg and kegg in kegg2formula else ''
-    for pw in sorted(pw2r_ids.iterkeys(), key=pw2pw_name):
-        r_ids = pw2r_ids[pw]
-        if not r_ids:
-            continue
-        pw_name = pw2pw_name(pw)
-        pw = pw_name if pw_name else pw
-        pw = pw[:min(31, len(pw))]
-        pw = ''.join(e for e in pw if e.isalnum() or e.isspace())
-        # ws = wb.create_sheet(title=pw)
-        # add_values(ws, 1, 1,
-        # ["Id", "Name", "Formula", "Kegg Formula", "Genes", "Kegg", "Ke(pH=7,T=298.15K)", "Low. B.", "Upp. B.", "New L. B.", "New Up. B.", "Comment"],#, "KEGG Formula", "Ke(pH=7,T=298.15K)"],
-        #            HEADER_STYLE)
-        # row = 2
-        data, styles = [], []
-        for r_id in sorted(r_ids):
-            r = model.getReaction(r_id)
-            lb, ub = get_bounds(r)
-            kegg = get_kegg_r_id(r)
-            result = compare_kegg_formulas(r, kegg2rs_ps, m2kegg)
-            r_lower_bound, r_upper_bound = lb, ub
-            ke = None
-            if result in [NO_KEGG_ID, NO_KEGG_INFO_FOUND]:
-                comment = "No KEGG formula found"
-            elif kegg in kegg2ke and result in [SUCCESS, REVERSED]:
-                ke = kegg2ke[kegg]
-                r_lower_bound, r_upper_bound = get_better_r_bounds(r, ke, result == REVERSED, 1000)
-                if result == REVERSED:
-                    comment = "Reversed KEGG formula"
-                else:
-                    comment = ""
-            else:
-                comment = "Different to KEGG formula"
-
-            style = RED_STYLE if lb != r_lower_bound or ub != r_upper_bound else BASIC_STYLE
-            styles.append(style)
-            data.append(
-                [r.id, r.name, get_sbml_r_formula(model, r, False), k_formula(kegg), get_gene_association(r), kegg, ke,
-                 lb, ub, r_lower_bound, r_upper_bound, comment])
-            # add_values(ws, row, 1, values)
-            # row += 1
-        save_data(["Id", "Name", "Formula", "Kegg Formula", "Genes", "Kegg", "Ke(pH=7,T=298.15K)", "Low. B.", "Upp. B.",
-                   "New L. B.", "New Up. B.", "Comment"],
-                  data=data, styles=styles, ws_name=pw, wb=wb)
     wb.save(path)
 
 
@@ -245,15 +154,11 @@ def save_metabolite_mappings(ws, source_model, target_model, m_id2m_id, m_id2m_i
 
 def save_reaction_mappings(ws, source_model, target_model, r_id2r_id, r_id2r_id_diff_comp,
                            s_r_ids_unmapped, r2kegg, r2bounds):
-    headers = ["Id", "Name", "Compartments", "Formula", "KEGG", "Genes", "Lower bound", "Upper bound",
-               "Ke (pH=7, T=298.15K)"]
+    headers = ["Id", "Name", "Compartments", "Formula", "KEGG", "Genes", "Lower bound", "Upper bound"]
     add_values(ws, 1, 2, [source_model.name if source_model.name else source_model.id], HEADER_STYLE)
     add_values(ws, 2, 2, headers, HEADER_STYLE)
     add_values(ws, 1, 2 + len(headers), [target_model.name if target_model.name else target_model.id], HEADER_STYLE)
     add_values(ws, 2, 2 + len(headers), headers, HEADER_STYLE)
-
-    kegg2ke = get_r_kegg2equilibrium_const(ph=7)
-    ke = lambda kegg: kegg2ke[kegg] if kegg in kegg2ke else ''
 
     ws.cell(row=3, column=1).value = "Matching"
     i = 4
@@ -262,8 +167,8 @@ def save_reaction_mappings(ws, source_model, target_model, r_id2r_id, r_id2r_id_
         s = Style(font=Font(color=openpyxl.styles.colors.BLACK))
         if s_r_id in r2bounds and t_r_id in r2bounds and r2bounds[s_r_id] != r2bounds[t_r_id]:
             s = Style(font=Font(color=openpyxl.styles.colors.RED))
-        add_reaction(ws, s_r_id, source_model, i, 2, r2kegg, r2bounds, ke, s)
-        add_reaction(ws, t_r_id, target_model, i, 2 + len(headers), r2kegg, r2bounds, ke, s)
+        add_reaction(ws, s_r_id, source_model, i, 2, r2kegg, r2bounds, s)
+        add_reaction(ws, t_r_id, target_model, i, 2 + len(headers), r2kegg, r2bounds, s)
         i += 1
 
     ws.cell(row=i, column=1).value = "Similar in different compartments"
@@ -273,14 +178,14 @@ def save_reaction_mappings(ws, source_model, target_model, r_id2r_id, r_id2r_id_
         s = Style(font=Font(color=openpyxl.styles.colors.BLACK))
         if s_r_id in r2bounds and t_r_id in r2bounds and r2bounds[s_r_id] != r2bounds[t_r_id]:
             s = Style(font=Font(color=openpyxl.styles.colors.RED))
-        add_reaction(ws, s_r_id, source_model, i, 2, r2kegg, r2bounds, ke, s)
-        add_reaction(ws, t_r_id, target_model, i, 2 + len(headers), r2kegg, r2bounds, ke, s)
+        add_reaction(ws, s_r_id, source_model, i, 2, r2kegg, r2bounds, s)
+        add_reaction(ws, t_r_id, target_model, i, 2 + len(headers), r2kegg, r2bounds, s)
         i += 1
 
     ws.cell(row=i, column=1).value = "Unmapped"
     i += 1
     for s_r_id in sorted(s_r_ids_unmapped):
-        add_reaction(ws, s_r_id, source_model, i, 2, r2kegg, r2bounds, ke)
+        add_reaction(ws, s_r_id, source_model, i, 2, r2kegg, r2bounds)
         i += 1
 
 
@@ -291,7 +196,7 @@ def add_metabolite(ws, m_id, model, row, col, m2kegg, m2chebi):
     add_values(ws, row, col, values)
 
 
-def add_reaction(ws, r_id, model, row, col, r2kegg, r2bounds, ke,
+def add_reaction(ws, r_id, model, row, col, r2kegg, r2bounds,
                  style=Style(font=Font(color=openpyxl.styles.colors.BLACK))):
     r = model.getReaction(r_id)
     cs = ", ".join(sorted((model.getCompartment(c_id).name for c_id in get_r_comps(r_id, model))))
@@ -301,9 +206,6 @@ def add_reaction(ws, r_id, model, row, col, r2kegg, r2bounds, ke,
         u_b, l_b = r2bounds[r_id]
         values.append(l_b)
         values.append(u_b)
-    ke = ke(kegg)
-    if ke:
-        values.append(ke)
     add_values(ws, row, col, values, style)
 
 
--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setup(name='mod_sbml',
           'Topic :: Systems Biology',
           'Topic :: Software Development',
       ],
-      install_requires=['openpyxl', 'python-libsbml-experimental', 'numpy', 'matplotlib', 'gmpy', 'cobra', 'networkx']
+      install_requires=['openpyxl', 'python-libsbml-experimental', 'numpy', 'matplotlib']
 )
