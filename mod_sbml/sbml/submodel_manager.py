85d535b8fda5bba232107f18601ee99a5b88a28a
annazhukova <zhutchok@gmail.com>
2015-09-18 10:03:37 +0200

    Minor improvements to the constraint-based analysis pipeline

--- mod_sbml/sbml/submodel_manager.py
+++ mod_sbml/sbml/submodel_manager.py
@@ -1,4 +1,5 @@
-from mod_sbml.sbml.sbml_manager import get_metabolites, get_reactants, get_products, get_modifiers
+from collections import Counter
+from mod_sbml.sbml.sbml_manager import get_metabolites, get_reactants, get_products, get_modifiers, generate_unique_id
 from mod_sbml.sbml.ubiquitous_manager import get_ubiquitous_chebi_ids, \
     select_metabolite_ids_by_term_ids
 
@@ -8,12 +9,16 @@ __author__ = 'anna'
 def submodel(r_ids_to_keep, model):
     for r_id in [r.id for r in model.getListOfReactions() if r.id not in r_ids_to_keep]:
         model.removeReaction(r_id)
+    remove_unused_species(model)
+    remove_unused_compartments(model)
+
+
+def remove_unused_species(model):
     s_ids_to_keep = set()
     for r in model.getListOfReactions():
         s_ids_to_keep |= get_metabolites(r, include_modifiers=True)
     for s_id in [s.id for s in model.getListOfSpecies() if s.id not in s_ids_to_keep]:
         model.removeSpecies(s_id)
-    remove_unused_compartments(model)
 
 
 def remove_unused_compartments(model):
@@ -76,3 +81,32 @@ def remove_species(model, s_ids_to_remove):
         model.removeSpecies(s_id)
     remove_unused_compartments(model)
 
+
+def create_lumped_reaction(r_id2coeff, model, id_prefix='rl_', zero_threshold=1e-3):
+    new_r_id = generate_unique_id(model, id_=id_prefix)
+    m_id2stoichiometry = compress_reaction_participants(model, r_id2coeff, zero_threshold=zero_threshold)
+    new_r = model.createReaction()
+    new_r.setId(new_r_id)
+    new_r.setName('lumped reaction: %s' % ''.join('%+g%s' % (coeff, r_id) for (r_id, coeff) in r_id2coeff.iteritems()))
+    new_r.setReversible(False)
+    for m_id, st in m_id2stoichiometry.iteritems():
+        if st < 0:
+            sr = new_r.createReactant()
+            sr.setSpecies(m_id)
+            sr.setStoichiometry(-st)
+        elif st > 0:
+            sr = new_r.createProduct()
+            sr.setSpecies(m_id)
+            sr.setStoichiometry(st)
+    for r_id in r_id2coeff.iterkeys():
+        model.removeReaction(r_id)
+    return new_r_id
+
+
+def compress_reaction_participants(model, r_id2coeff, zero_threshold=1e-3):
+    m_id2stoichiometry = Counter()
+    for r_id, coeff in r_id2coeff.iteritems():
+        r = model.getReaction(r_id)
+        m_id2stoichiometry.update({m_id: -st * coeff for (m_id, st) in get_reactants(r, stoichiometry=True)})
+        m_id2stoichiometry.update({m_id: st * coeff for (m_id, st) in get_products(r, stoichiometry=True)})
+    return {m_id: st for (m_id, st) in m_id2stoichiometry.iteritems() if abs(st) > zero_threshold}
