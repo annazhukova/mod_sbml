bc6168883c9ed9bdcfe036c47c8f26d6de5277c0
Anna Zhukova <zhutchok@gmail.com>
2017-03-31 10:52:00 +0200

    Updated setup files: included README.md file

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     packages=['mod_sbml'],
     package_data={'mod_sbml': [os.path.join('data', '*.obo'), os.path.join('data', '*.csv'),
                                os.path.join('data', '*.txt'), os.path.join('*', '*.py'),
-                               os.path.join('*', '*', '*.py')]},
+                               os.path.join('*', '*', '*.py'),
+                               os.path.join('..', 'README.md')]},
+    long_description=open('README.md').read(),
     include_package_data=True,
     platform=['MacOS', 'Linux', 'Windows'],
     classifiers=[
@@ -17,12 +19,12 @@ setup(
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    version='0.2.2',
+    version='0.2.3',
     description='Utilities for working with metabolic models in SBML format.',
     author='Anna Zhukova',
     author_email='zhutchok@gmail.com',
     url='https://github.com/annazhukova/mod_sbml',
-    download_url='https://github.com/annazhukova/mod_sbml/archive/0.2.2.zip',
+    download_url='https://github.com/annazhukova/mod_sbml/archive/0.2.3.zip',
     keywords=['SBML', 'metabolic model', 'utility'],
     install_requires=['openpyxl', 'python-libsbml-experimental', 'pandas', 'matplotlib', 'pyparsing', 'natsort']
 )
